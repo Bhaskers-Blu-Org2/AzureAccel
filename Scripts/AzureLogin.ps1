b999d7be091405d83fcf8b79086373e8c095b67f
Marc-Andre Morisset <mamoris@microsoft.com>
2018-01-04 19:28:15 +0000

    initial commit

---
+++ Scripts/AzureLogin.ps1
@@ -0,0 +1,8 @@
+#Interactive log in
+#Login-AzureRmAccount. You will get dialog box asking for your Azure credentials.
+#Type the email address and password associated with your account. 
+#Azure authenticates and saves the credential information, and then closes the window.
+
+Login-AzureRmAccount
+
+
