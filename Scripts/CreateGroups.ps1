2e0227e631d0ced33615036ba8d746d3a316f0c3
Marc-Andre Laniel <malaniel@microsoft.com>
2018-01-09 14:34:18 -0500

    Push

--- Scripts/CreateGroups.ps1
+++ Scripts/CreateGroups.ps1
@@ -1,11 +1,26 @@
 #Param(
 #    [Parameter(Mandatory=$true)][string]$InputFile
 #    ) 
-##TEMP BELOW
-$InputFile = "C:\Users\malaniel\GitRepos\AzureAccel\Scripts\UserList.csv"
+##
+### This script reads the user csv file for the userid
+### It creates groups in Azure AD for those roles
+### 
 
-$users = Import-Csv $InputFile
+$sub = Get-AzureRmSubscription
 
-$filter = @("role", "role2", "role3")
-    $users |where-object $_.role -contains "Contributor"
+$BillingAdminGroupDisplayName = "Billing Readers"
+$BillingAdmingGroupDesc = "Users can read roles and role assignments,billing information and manage support tickets "
+
+$listroles = Get-AzureRmRoleDefinition
+
+
+$billingusers = $userlist | where {$_.role -match "Billing"}
+$BillingGroup = New-AzureADGroup -Description $BillingAdmingGroupDesc -DisplayName $BillingAdminGroupDisplayName -MailEnabled $false -SecurityEnabled $true -Verbose -MailNickName "Billing-Admins"
+$billingRole = $listroles |where {$_.Name -match "Billing Reader"}
+$newbillingroleAssignment = New-AzureRmRoleAssignment -ObjectId $BillingGroup.ObjectId -Scope ("/subscriptions/" + $sub.id) -RoleDefinitionId $BillingRole.Id -Verbose
+$billingusers | ForEach-Object{
+Add-AzureADGroupMember -ObjectId $BillingGroup.ObjectId -RefObjectId $_.ObjectId
+                        }
+
+
 
