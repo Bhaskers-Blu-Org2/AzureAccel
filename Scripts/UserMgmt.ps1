d28054427b2981ed46acb228c3ff5f78fe24e418
Marc-Andre <35046567+malaniel@users.noreply.github.com>
2018-01-09 16:00:15 -0500

    Alpha (#6)
    * First Commit on Alpha Branch
    
    * Scripts Folder and Resource Group Creation Script
    
    * PolicyScripts
    
    * Tags2
    
    * Policy3
    
    * End of Day
    
    * Deleted Old Script
    
    * UserMgmt Script and Input File
    
    * gitignore changes
    
    * User List Addition
    
    * Remove KeyVault mandatory
    
    * new usermgmt without keyvault
    
    * New UserMgmt Script
    
    * Push
    
    * Groups Update

--- Scripts/UserMgmt.ps1
+++ Scripts/UserMgmt.ps1
@@ -9,9 +9,11 @@ Param(
 $PasswordLength = 12
 $NonAlphCh = 4 
 Add-Type -AssemblyName System.Web
-Get-AzureRmKeyVault -VaultName $KeyVaultName
+#$KeyVaultName = "ml11keyvault"
+#Get-AzureRmKeyVault -VaultName $KeyVaultName
 #Importing CSV file and starting iteration through accounts
-Import-Csv $InputFile |ForEach-Object{
+$userlist = Import-Csv $InputFile 
+$userlist |ForEach-Object{
 
 
 $postalcode = "K1P5B4"
@@ -20,8 +22,8 @@ $PasswordProfile.Password = ([System.Web.Security.Membership]::GeneratePassword(
 $mailnnickname = $_.givenname + $_.surname
 $displayname = $_.givenname + " " + $_.surname
 $upn = $_.givenname + "." + $_.surname + "@mlaniel11outlook.onmicrosoft.com"
-$usertype = "Member"
 $newuser = New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $true -DisplayName $displayname -Department $_.department -GivenName $_.givenname -Surname $_surname -UserPrincipalName $upn -MailNickName $mailnnickname -PostalCode $postalcode -Verbose
+$_.ObjectID = $newuser.ObjectID
 ## Determining the user group and adding them to the right group
 # To be added later
 ##############################
@@ -29,10 +31,12 @@ $newuser = New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $tr
 # For this to work, you will need to specify the name of a key vault in your sub that you can access at script launch
 # If you do not have a key vault or do not wish to use it, you can safely delete the three lines below.
 ########################
-#$secretname = $mailnnickname + "PW"
+##$secretname = $mailnnickname + "PW"
 #$secretvalue = ConvertTo-SecureString $PasswordProfile.Password -AsPlainText -Force
 #$secret = Set-AzureKeyVaultSecret -VaultName $keyvault.VaultName -Name $secretname  -SecretValue $secretvalue -ContentType "Initial Password"
 
 
 }
+
+Export-Csv -InputObject $userlist .\userlist2.CSV
 
